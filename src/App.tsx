99e70d776dde0dbda75e630c948a99a93e62e7c7
Queñano Jhon Marcellino <okayda999@gmail.com>
2024-08-06 17:53:55 -1200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import KanbanBoard from "./components/KanbanBoard";
+
+function App() {
+  return (
+    <main className="antialiased ">
+      <KanbanBoard />
+    </main>
+  );
+}
+
+export default App;
